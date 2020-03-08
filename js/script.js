fe74543869557ec84ead0622d13e8f05b9f4be47
Bruno Mendanha <bmendanha@hotmail.com>
2020-03-08 18:48:19 +0000

    ‘commit’

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var hotel = {
     // Object properties
-    name: 'Clayton',
+    name: 'Mendanha',
     rooms: 110,
     booked: 25,
     gym: true,
@@ -17,28 +17,35 @@ var elRooms = document.getElementById('rooms');
 elRooms.textContent = hotel.checkAvailablity();
 
 // Define a constructor function template for hotels
-function Hotel(name, rooms, booked) {
+function Hotel(name, rooms, booked, gym) {
     // Object properties
     this.name = name;
     this.rooms = rooms;
     this.booked = booked;
+    this.gym = gym;
     this.checkAvailablity = function() {
         return this.rooms - this.booked;
     };
 }
 
 // Define to instances of the hotel and their arguments
-var clayHotel = new Hotel('Clayton', 68, 3);
-var lemonHotel = new Hotel('Lemon', 56, 15);
+var mendanhaspa = new Hotel('Mendanha', 110, 25, true);
+var lemonHotel = new Hotel('Lemon', 56, 15, false);
 
 // Declare a variable to hold the name of the hotel and room availability 
-var details1 = clayHotel.name + ' rooms: ';
+var details1 = mendanhaspa.name + ' rooms: ';
     // add content to an existing variable----
-    details1 += clayHotel.checkAvailablity();
+    details1 += mendanhaspa.checkAvailablity();
+var details1_gym = 'Gym: ' + mendanhaspa.gym;
+var details1_rooms = 'Total rooms: ' + mendanhaspa.rooms
 // Get the hotel1 div
 var elHotel1 = document.getElementById('hotel1');
+var elHotel1_gym = document.getElementById('hotel1_gym');
+var elHotel1_rooms = document.getElementById('hotel1_rooms');
 // Add the variables created onto the empty div
 elHotel1.textContent = details1;
+elHotel1_gym.textContent = details1_gym;
+elHotel1_roms.textContent = details1_rooms;
 
 var details2 = lemonHotel.name + ' rooms: ';
     details2 += lemonHotel.checkAvailablity();
